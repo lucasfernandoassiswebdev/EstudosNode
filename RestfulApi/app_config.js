34743e2caf64346d7f6e9b546358a84c65ccd318
Lucas Fernando <lucasaassis@outlook.com>
2018-05-22 10:58:37 -0300

    Adicionando CORS validation

--- RestfulApi/app_config.js
+++ RestfulApi/app_config.js
@@ -2,7 +2,18 @@ var express = require('express');
 var app = module.exports = express();
 var bodyParser = require('body-parser');
 
+var allowCors = function (req, res, next) {  
+    res.header('Access-Control-Allow-Origin','127.0.0.1:5000'); //Domínios autorizados a acessar a informação
+    res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers','Content-type');
+    res.header('Access-Control-Allow-Credentials','true');
+
+    next();
+}
+
 app.listen(5000);
+
+app.use(allowCors);
 
 app.use(bodyParser.json());
 
